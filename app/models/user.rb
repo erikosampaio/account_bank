3dd2609914cd8c581e74feccc75e480eaa5c61be
erikosampaio <erikoa.93@gmail.com>
2022-08-13 18:12:39 -0300

    Crio tabela conta e faço migração

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  has_one :account
+
+  accepts_nested_attributes_for :account, reject_if: :all_blank, allow_destroy: false
+
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
---
+++ db/migrate/20220813210919_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :accounts do |t|
+      t.integer :number
+      t.float :balance
+      t.boolean :situation
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
